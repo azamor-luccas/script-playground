61f8edc7862540574be2dc360f32d50e1d681e65
Luccas Azamor <luccas.azamor@gmail.com>
2024-03-19 00:21:48 -0300

    feat: create main

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+const Repl = require("repl");
+import loadConfigFile from "./config/loadUserConfigFile";
+import { initializeAppDataSource } from "./helpers/initializeAppDataSource";
+import Imports from "./helpers/imports";
+import { Config } from "./config/config";
+import { loadDependencies } from "./helpers/loadDependencies";
+import { REPLInitializer } from "./repl";
+
+async function main() {
+  const config: Config = await loadConfigFile();
+  const importsHelper: Imports = new Imports(config.env);
+
+  require("dotenv").config({ path: config.env.ENV_PATH });
+
+  const [dataSource, dependencies] = await Promise.all([
+    initializeAppDataSource(config?.datasource),
+    loadDependencies(),
+  ]);
+
+  new REPLInitializer(
+    config,
+    importsHelper,
+    Repl,
+    dataSource,
+    dependencies
+  ).initREPL();
+}
+
+main();
